30ccae15f640b709b37bff3bc4a4d54c14f4c824
salimi <salimiali941@gmail.com>
2023-08-14 17:09:28 +0330

    product model and view and template

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'home'
 urlpatterns = [
 	path('', views.HomeView.as_view(), name='home'),
-]
+    path('<slug:slug>/',views.ProductsDetailView.as_view(),name='product_detail')
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
